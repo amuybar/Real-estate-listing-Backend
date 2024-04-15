63219ed2635c53dc248835e03782c920335570f5
amuybar <amuyunzubq@gmail.com>
2024-04-15 12:21:05 +0300

    ..

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
-const express =require("express");
-const cookie =require('cookie-parser');
-const cors=require('cors');
+const express = require("express");
+const cookie = require('cookie-parser');
+const cors = require('cors');
 
+const mongoose = require("mongoose");
 
-const mongoose=require("mongoose");
+// Require environment variables from .env file
+require('dotenv').config();
+
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 const propertyRoutes = require('./routes/property');
 const agentRoutes = require('./routes/agent');
 const testimonialRoutes = require('./routes/testmonial');
 
-const app=express();
+const app = express();
 app.use(express.json());
 app.use(cookie());
 app.use(cors());
 
-
+// Connect to MongoDB using environment variable
+const mongoURI = process.env.MONGO_URI;
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Error connecting to MongoDB:', err));
 
 app.use('/auth', authRoutes);
 app.use('/user', userRoutes);
@@ -23,15 +33,6 @@ app.use('/properties', propertyRoutes);
 app.use('/agents', agentRoutes);
 app.use('/testimonials', testimonialRoutes);
 
-
-mongoose.connect("mongodb+srv://Barry:Amuy%232000@cluster.3kuya2i.mongodb.net/").then(() => {
-  console.log('Connected to MongoDB');
-}).catch(err => {
-  console.error('Error connecting to MongoDB:', err);
-});
-
-
-
-app.listen(3000,()=>{
+app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
